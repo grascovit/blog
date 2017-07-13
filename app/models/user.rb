1401c7e39b8ee06c65eebae1bc73846708a88b3f
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-07-13 14:11:09 -0300

    add avatar to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  has_attached_file :avatar,
+                    styles: { medium: '300x300>', thumb: '100x100>' },
+                    default_url: ':style/missing-avatar.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
 end
