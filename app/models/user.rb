e1c64a1a00e30df36c257f6c2cb601938365750e
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-07-16 10:52:51 -0300

    create method to return user full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
                     styles: { medium: '300x300>', thumb: '100x100>' },
                     default_url: ':style/missing-avatar.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
+
+  def full_name
+    "#{first_name} #{last_name}"  
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
   end
+
+  describe 'instance methods' do
+    describe '#full_name' do
+      it 'should return the first and the last name in a string' do
+        user = create(:user)
+        first_name = user.first_name
+        last_name = user.last_name
+
+        expect(user.full_name).to eq "#{first_name} #{last_name}"
+      end
+    end
+  end
 end
