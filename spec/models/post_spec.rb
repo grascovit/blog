e309e6b1e774ef5e5cba8834c28205fbb9ce0cbe
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-07-16 12:06:27 -0300

    create scope to sort posts by creation date (desc)

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Post, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:message) }
   end
+
+  describe 'scopes' do
+    describe '.by_created_date' do
+      it 'returns the posts ordered by descending creation date' do
+        old_post = create(:post)
+        middle_post = create(:post)
+        new_post = create(:post)
+        ordered_posts = Post.by_created_date
+
+        expect(ordered_posts.first).to eq new_post
+        expect(ordered_posts.second).to eq middle_post
+        expect(ordered_posts.last).to eq old_post
+      end
+    end
+  end
 end
