c64ac521e41fa245a74d13921b232a88d06adc9f
Gabriel Rascovit <rascovit@gmail.com>
2017-08-15 23:35:58 -0300

    add pg_search required extensions

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728020617) do
+ActiveRecord::Schema.define(version: 20170816021622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "fuzzystrmatch"
 
   create_table "notifications", force: :cascade do |t|
     t.text "message", default: "", null: false
